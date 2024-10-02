3d53069a5a9fd0ce5f2740363aaed04998242031
Philipp Memmel <philipp.memmel@gmail.com>
2024-10-02 11:09:23 +0200

    Again codestyle

--- classes/local/userstats_table.php
+++ classes/local/userstats_table.php
@@ -109,7 +109,7 @@ class userstats_table extends table_sql {
             $where = 'u.' . $tenantfield . ' = :tenant GROUP BY u.id ORDER BY u.lastname';
             $params = ['tenant' => $tenant->get_sql_identifier()];
             $this->set_count_sql(
-                    "SELECT COUNT(DISTINCT u.id) FROM $from WHERE u." . $tenantfield . " = :tenant",
+                    "SELECT COUNT(DISTINCT u.id) FROM " . $from . " WHERE u." . $tenantfield . " = :tenant",
                     $params
             );
         }
