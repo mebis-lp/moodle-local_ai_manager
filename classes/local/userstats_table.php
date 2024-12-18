9184877b6ee004d4490fb7ec9c663efe1cc46c07
Philipp Memmel <philipp.memmel@gmail.com>
2024-12-18 23:11:53 +0100

    MBS-9805: Fix GROUP BY SQL statements to work with postgres

--- classes/local/userstats_table.php
+++ classes/local/userstats_table.php
@@ -94,7 +94,7 @@ class userstats_table extends table_sql {
             $from = '{local_ai_manager_request_log} rl '
                     . 'LEFT JOIN {local_ai_manager_userinfo} ui ON rl.userid = ui.userid '
                     . 'JOIN {user} u ON u.id = rl.userid';
-            $where = $tenantfield . ' = :tenant AND purpose = :purpose GROUP BY u.id';
+            $where = $tenantfield . ' = :tenant AND purpose = :purpose GROUP BY u.id, lastname, firstname, locked';
             $params = [
                     'tenant' => $tenant->get_sql_identifier(),
                     'purpose' => $purpose,
@@ -107,7 +107,7 @@ class userstats_table extends table_sql {
         } else {
             $fields = 'u.id as id, lastname, firstname, COUNT(value) AS requestcount';
             $from = '{local_ai_manager_request_log} rl LEFT JOIN {user} u ON rl.userid = u.id';
-            $where = 'u.' . $tenantfield . ' = :tenant GROUP BY u.id';
+            $where = 'u.' . $tenantfield . ' = :tenant GROUP BY u.id, lastname, firstname';
             $params = ['tenant' => $tenant->get_sql_identifier()];
             $this->set_count_sql(
                     "SELECT COUNT(DISTINCT u.id) FROM " . $from . " WHERE u." . $tenantfield . " = :tenant",
