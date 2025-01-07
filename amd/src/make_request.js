3d7fc137148a181044e92e1d0d828e3dae421734
Philipp Memmel <philipp.memmel@gmail.com>
2025-01-07 10:34:53 +0100

    MBS-9808: Make providing of component and context required

--- amd/src/make_request.js
+++ amd/src/make_request.js
@@ -2,31 +2,41 @@ import {call as fetchMany} from 'core/ajax';
 
 /**
  * Call to store input value
- * @param {string} purpose
- * @param {string} prompt
- * @param {array} options
- * @returns {mixed}
+ * @param {string} purpose the purpose to use for the request
+ * @param {string} prompt the prompt of the request
+ * @param {string} component the component from which the request is being done
+ * @param {number} contextid the id of the context from which the request is being done, or 0 for system context
+ * @param {string} options additional options, as json encoded string, or an empty string if no additional options
+ * @returns {Promise} the request promise
  */
 const execMakeRequest = (
     purpose,
     prompt,
+    component,
+    contextid,
     options
 ) => fetchMany([{
     methodname: 'local_ai_manager_post_query',
     args: {
         purpose,
         prompt,
+        component,
+        contextid,
         options
     },
 }])[0];
 
 /**
  * Executes the call to store input value.
- * @param {string} purpose
- * @param {string} prompt
- * @param {array} options
+ *
+ * @param {string} purpose the purpose to use for the request
+ * @param {string} prompt the prompt of the request
+ * @param {string} component the component from which the request is being done
+ * @param {number} contextid the id of the context from which the request is being done,
+ *  will default to 0 (which means system context)
+ * @param {object} options additional options
  * @returns {mixed}
  */
-export const makeRequest = async(purpose, prompt, options = {}) => {
-    return execMakeRequest(purpose, prompt, JSON.stringify(options));
+export const makeRequest = async(purpose, prompt, component, contextid = 0, options = {}) => {
+    return execMakeRequest(purpose, prompt, component, contextid, JSON.stringify(options));
 };
